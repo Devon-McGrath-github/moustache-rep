8e3113f96a2a9e1b01e51052ad65b79a6d395c95
Devon-McGrath-github <devon.mcgrath@gmail.com>
2024-05-14 18:35:45 +1200

    Adjust styling to better match requirements/prototype

--- src/components/productPage.tsx
+++ src/components/productPage.tsx
@@ -12,7 +12,7 @@ export default function ProductPage({
   addToCart,
 }: Product & { addToCart: (product: Selection) => void }) {
   const [count, setCount] = useState(0);
-  const [size, setSize] = useState('S');
+  const [size, setSize] = useState('');
   const [quantity, setQuantity] = useState(1);
 
   return (
@@ -23,13 +23,18 @@ export default function ProductPage({
         className="mx-auto h-auto max-w-full max-h-[26rem] md:max-h-2/3 md:w-1/3"
       />
       <div className="flex flex-col gap-2">
-        <h1>
+        <h1 className="text-lg">
           <strong>{title}</strong>
         </h1>
-        <h3>${price}</h3>
+        <h3 className="text-[#222222]">
+          <strong>${price}</strong>
+        </h3>
         <p>{description}</p>
 
         <div className="flex flex-col gap-2 ">
+          <h2>
+            Size & Quantity<span className="text-[#C90000]">*</span>
+          </h2>
           <div className="flex gap-2">
             {/* add validation to input - prevent submission of values outside min/max */}
             <Input
@@ -39,7 +44,7 @@ export default function ProductPage({
               // max="99"
               placeholder="1"
               onChange={(e) => setQuantity(parseInt(e.target.value))}
-              className="w-12"
+              className={'w-12'}
             />
 
             {sizes.map((e) => (
@@ -47,7 +52,7 @@ export default function ProductPage({
                 variant="outline"
                 key={e}
                 onClick={() => setSize(e)}
-                className={e === size ? 'border-black' : ''}
+                className={e === size ? 'border-black' : '' + ''}
               >
                 {e}
               </Button>
@@ -55,6 +60,7 @@ export default function ProductPage({
           </div>
 
           <Button
+            variant="outline"
             onClick={() => {
               setCount(count + 1);
               addToCart({
@@ -67,9 +73,11 @@ export default function ProductPage({
                 quantity,
               });
             }}
-            className="w-full max-w-[12.6rem]"
+            className={
+              'w-full max-w-[12.6rem] border-black border-2 text-[#222222] hover:bg-[#222222] hover:text-white duration-200'
+            }
           >
-            Add to Cart
+            <strong>Add to Cart</strong>
           </Button>
         </div>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import './styles.css';
 
 import Root from './routes/root.tsx';
 
